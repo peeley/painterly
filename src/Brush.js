590e069c1d3341773538d7aca747f50f6f139cab
Noah Snelson <noah.snelson@protonmail.com>
2020-02-17 19:12:31 -0800

    Changed Brush to use canvas path/line tools instead of drawing rectangles

--- src/Brush.js
+++ src/Brush.js
@@ -1,23 +1,32 @@
 export class Brush {
     constructor(){
         this.mouseDown = false;
-        this.dotSize = 10;
+        this.dotSize = 3;
         this.handleEvent = this.handleEvent.bind(this);
+        this.lastX = null;
+        this.lastY = null;
+        this.joinType = 'round';
     }
     handleEvent(event, context){
         if(event.type === "mousedown"){
             this.mouseDown = true;
-            let mouseX = event.clientX;
-            let mouseY = event.clientY;
-            context.fillRect(mouseX, mouseY, this.dotSize, this.dotSize);
+            this.lastX = event.clientX;
+            this.lastY = event.clientY;
         }
         else if(event.type === "mouseup" || event.type === "mouseleave"){
             this.mouseDown = false;
         }
         else if(this.mouseDown && event.type === "mousemove"){
+            context.linewidth = this.dotSize;
+            context.lineJoin = this.joinType;
             let mouseX = event.clientX;
             let mouseY = event.clientY;
-            context.fillRect(mouseX, mouseY, this.dotSize, this.dotSize);
+            context.beginPath();
+            context.moveTo(this.lastX, this.lastY);
+            context.lineTo(mouseX, mouseY);
+            context.stroke();
+            this.lastX = mouseX;
+            this.lastY = mouseY;
         }
     }   
 }

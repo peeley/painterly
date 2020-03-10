9315b5f4e304a4057b8973f516c45c3bb4212b6e
Noah Snelson <noah.snelson@protonmail.com>
2020-03-09 22:58:38 -0700

    Change architecture for more one-way data flow in ToolController, need to fix null reference to drawSurface

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,51 +1,39 @@
 import React from 'react';
 import './Canvas.css';
-import { Palette } from './Palette.js';
 import { ToolController } from './ToolController.js';
-import { ToolList } from './ToolList.js';
 
 class Canvas extends React.Component{
     constructor(props){
         super(props);
-        this.drawSurfaceRef = React.createRef();
-        this.updateToolColor = this.updateToolColor.bind(this);
-        this.updateStrokeWidth = this.updateStrokeWidth.bind(this);
         this.handleInput = this.handleInput.bind(this);
+        this.handleToolSelect = this.handleToolSelect.bind(this);
         this.state = {
-            toolController: { selectNewTool: () => {}}
+            drawSurface: React.createRef()
         };
     }
     componentDidMount(){
-        this.drawSurfaceCtx = this.drawSurfaceRef.current.getContext('2d');
-        this.setState({
-            toolController: new ToolController(this.drawSurfaceRef)
-        });
         document.addEventListener('keydown', (event) => {
             if(event.ctrlKey && event.key === 'z'){
                 // TODO : create undo method, clean up keypress listener
             }
         });
     }
-    updateToolColor(color){
-        this.state.toolController.setColor(color);
-    }
-    updateStrokeWidth(width){
-        this.state.toolController.setStrokeWidth(width);
+    handleToolSelect(tool){
+        this.setState({
+            tool: tool
+        });
     }
     handleInput(event){
-        this.state.toolController.handleEvent(event, this.drawSurfaceCtx);
+        this.state.tool.handleEvent(event, this.state.drawSurface);
         event.preventDefault();
     }
     render(){
         return(
             <div className="Canvas">
-                <div className="controlBar">
-                    <Palette 
-                        updateStrokeWidth={this.updateStrokeWidth}
-                        updateColor={this.updateToolColor}
-                    />
-                    <ToolList selectTool={this.state.toolController.selectNewTool}/>
-                </div>
+                <ToolController 
+                    surface={this.state.drawSurface}
+                    handleToolSelect={this.handleToolSelect}
+                />
                 <canvas id="drawSurface" 
                     onMouseDown = {this.handleInput}
                     onMouseMove = {this.handleInput}                    
@@ -53,7 +41,7 @@ class Canvas extends React.Component{
                     onMouseUp = {this.handleInput}
                     height = { window.innerHeight * .85 }
                     width = { window.innerWidth * .9 }
-                    ref={ this.drawSurfaceRef }
+                    ref={ this.state.drawSurface }
                 />
             </div>
         )
--- src/ToolController.js
+++ src/ToolController.js
@@ -1,25 +1,57 @@
+import React from 'react';
+import { Palette } from './Palette.js';
 import { PenTool } from './PenTool.js';
 import { RectTool } from './RectTool.js';
 
-export class ToolController{
-    constructor(surfaceRef){
+export class ToolController extends React.Component{
+    constructor(props){
+        super(props);
+        this.selectNewTool = this.selectNewTool.bind(this);
+        this.state = {
+            selected: "pen"
+        };
+    }
+    handleChange(event){
+        let toolName = event.target.value;
+        this.setState({
+            selected: toolName
+        });
+        this.props.selectTool(toolName);
+    }
+    componentDidMount(){
         this.toolSet = {
-            'pen': new PenTool(surfaceRef),
-            'rect': new RectTool(surfaceRef)
+            'pen': new PenTool(this.props.surface),
+            'rect': new RectTool(this.props.surface)
         }
         this.selectedTool = this.toolSet['pen'];
-        this.selectNewTool = this.selectNewTool.bind(this);
-    }
-    handleEvent(event, context){
-        this.selectedTool.handleEvent(event, context);
     }
     selectNewTool(toolName){
         this.selectedTool = this.toolSet[toolName];
+        this.props.handleToolSelect(this.selectedTool);
     }
     setStrokeWidth(width){
         this.selectedTool.setStrokeWidth(width);
     }
     setColor(color){
         this.selectedTool.setColor(color);
+    }
+    render(){
+        return(
+            <div className="controlBar">
+                <Palette 
+                    updateStrokeWidth={this.setStrokeWidth}
+                    updateColor={this.setColor}
+                />
+                <input type="radio" value="pen" id="pen" 
+                    checked={this.state.selected === "pen"} 
+                    onChange={this.handleChange} />
+                <label htmlFor="pen"> Pen </label>
+
+                <input type="radio" value="rect" id="rect" 
+                    checked={this.state.selected === "rect"} 
+                    onChange={this.handleChange} />
+                <label htmlFor="rect"> Rect </label>
+            </div>
+        );
     }
 }

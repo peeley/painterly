6ec1bae0782fd28496938d8eddb21949f0bbba8a
Noah Snelson <noah.snelson@protonmail.com>
2020-03-15 22:06:16 -0700

    Moved React app into Laravel, started work on backend

---
+++ database/migrations/2020_03_16_032323_create_paintings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaintingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paintings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->timestamps();
+            $table->string('title');
+            $table->json('strokes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paintings');
+    }
+}

271b3dcf12f17372802b517894d744a5f5aa635a
Noah Snelson <noah.snelson@protonmail.com>
2020-03-22 20:56:04 -0700

    Added SQL expression in migration to mitigate misformed models

--- database/migrations/2020_03_16_032323_create_paintings_table.php
+++ database/migrations/2020_03_16_032323_create_paintings_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Query\Expression;
 use Illuminate\Support\Facades\Schema;
 
 class CreatePaintingsTable extends Migration
@@ -18,7 +19,7 @@ class CreatePaintingsTable extends Migration
             $table->foreignId('user_id');
             $table->timestamps();
             $table->string('title')->default('Blank painting');
-            $table->json('strokes')->default(json_encode([]));
+            $table->json('strokes')->default(new Expression('(json_array())'));
         });
     }
 

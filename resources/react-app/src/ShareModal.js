98a80abeabc3d71e0fb4c919a74b91280f9dd86e
Noah Snelson <noah.snelson@protonmail.com>
2020-03-28 14:14:20 -0700

    Added alert when link is copied

--- resources/react-app/src/ShareModal.js
+++ resources/react-app/src/ShareModal.js
@@ -5,10 +5,16 @@ export class ShareModal extends React.Component {
         super(props);
         this.shareLink = window.location.href;
         this.linkTextArea = React.createRef();
+        this.state = {
+            copied: false
+        };
     }
     copyLink = () => {
         this.linkTextArea.current.select();
         document.execCommand('copy');
+        this.setState({
+            copied: true
+        });
     }
     render(){
         return (
@@ -18,25 +24,33 @@ export class ShareModal extends React.Component {
                     Share Link
                 </button>
                 <div id="shareModal" className="modal fade">
-                    <div className="modal-dialog modal-dialog-centered" 
+                    <div className="modal-dialog" 
                         role="document">
                         <div className="modal-content">
                             <div className="modal-header">
                                 <h3>Share Link</h3>
                             </div>
                             <div className="row modal-body">
-                                <input type="text" value={this.shareLink} 
+                                <input className="col-9" 
+                                    type="text" value={this.shareLink} 
                                     ref={this.linkTextArea} readOnly />
-                                <button onClick={this.copyLink}>
+                                <button className="col-3" 
+                                    onClick={this.copyLink}>
                                     Copy
                                 </button>
                             </div>
                             <div className="modal-footer">
-                                <button type="button" data-dismiss="modal">
+                                <button type="button" data-dismiss="modal"
+                                    onClick={() => this.setState({ copied: false})} >
                                     Close
                                 </button>
                             </div>
                         </div>
+                        { this.state.copied ? (
+                            <div className="alert alert-success alert-dismissable fade show" role="alert">
+                                Copied to clipboard!
+                            </div>
+                        ) : '' }
                     </div>
                 </div>
             </>

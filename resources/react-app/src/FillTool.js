00e80850fa9a5e5dda17fa077e7e90a96837e3ec
Noah Snelson <noah.snelson@protonmail.com>
2020-04-08 00:09:07 -0700

    Further work on fill tool

--- resources/react-app/src/FillTool.js
+++ resources/react-app/src/FillTool.js
@@ -9,12 +9,30 @@ export class FillTool extends Tool {
         if(event.type === 'mousedown'){
             const xCoord = event.clientX - this.leftOffset;
             const yCoord = event.clientY - this.topOffset;
-            const imgPixels = context.getImageData(xCoord, yCoord, 1, 1).data;
-            console.log(imgPixels);
+            const background= context.getImageData(xCoord, yCoord, 1, 1).data;
+            this.floodFill(context, xCoord, yCoord, background);
         }
     }
 
-    floodFill = (pixels, x, y, backgroundColor) => {
+    floodFill = (context, x, y, backgroundColor) => {
         // TODO: floodfill algorithm
+        let imgData = context.getImageData(x, y, 1, 1);
+        let pixelColor = imgData.data;
+        if(!pixelColor){
+            return;
+        }
+        if(pixelColor === backgroundColor){
+            const newColor = this.hexToRGBA(this.color);
+            for(let i=0; i<pixelColor.length; i++){
+                pixelColor[i] = newColor[i];
+            }
+            context.putImageData(imgData, x, y);
+        }
+    }
+
+    hexToRGBA = (color) => {
+        let strRGBA = color.split('(')[1].split(')')[0].split(',')
+        // turns out + in JS is a unary operator for numeric casting, neat
+        return strRGBA.map( item => +item);
     }
 }

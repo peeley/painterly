56441a8211cd4269ace490315a295a3e0b4dbf93
Noah Snelson <noah.snelson@protonmail.com>
2020-04-10 20:26:34 -0700

    Basic view permission logic done for paintings

--- app/Http/Controllers/PaintingController.php
+++ app/Http/Controllers/PaintingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\DB;
 use \App\Painting;
 
 class PaintingController extends Controller
@@ -16,7 +17,18 @@ class PaintingController extends Controller
     }
     
     public function show(\App\Painting $painting){
-        return view('app', ['title' => $painting->title]);
+        if($painting->view_private){
+            $perms = DB::table('permissions')
+                ->select('permissions')
+                ->where([
+                    ['user_id', Auth::id()],
+                    ['painting_id', $painting->id]])
+                ->first();
+            if($perms || $painting->user_id != Auth::id()){
+                return view('app', ['title' => $painting->title]);
+            }
+        }
+        return response('Forbidden', 403);
     }
     public function getStrokes(\App\Painting $painting){
         return $painting;

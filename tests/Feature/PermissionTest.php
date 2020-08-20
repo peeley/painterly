b1bb558312875067802956be8ad62bca15efb889
Noah Snelson <noah.snelson@protonmail.com>
2020-08-20 16:45:50 -0700

    Add test for deleting Permission on painting

--- tests/Feature/PermissionTest.php
+++ tests/Feature/PermissionTest.php
@@ -3,9 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Permission;
 use App\User;
 
 class PermissionTest extends TestCase
@@ -37,11 +35,14 @@ class PermissionTest extends TestCase
         $painting = $this->testUser->paintings()->create();
         $painting->save();
 
-        $response = $this->actingAs($this->testUser)
-            ->getJson("/api/p/$painting->id/perms");
-
-        $response->assertStatus(200)
+        $this->actingAs($this->testUser)
+            ->getJson("/api/p/$painting->id/perms")
+            ->assertStatus(200)
             ->assertExactJson([]);
+
+        $this->actingAs($this->otherUser)
+            ->get("/api/p/$painting->id/perms")
+            ->assertStatus(403);
     }
 
     public function testAddPermission()
@@ -49,17 +50,41 @@ class PermissionTest extends TestCase
         $painting = $this->testUser->paintings()->create();
         $painting->save();
 
-        $add_user_response = $this->actingAs($this->testUser)
+        $this->actingAs($this->testUser)
+            ->postJson("/api/p/$painting->id/perms", [
+                'perms' => 'read_write',
+                'email' => $this->otherUser->email,
+            ])
+            ->assertStatus(200);
+
+        $this->actingAs($this->testUser)
+            ->getJson("/api/p/$painting->id/perms")
+            ->assertStatus(200)
+            ->assertJsonFragment([
+                'user_id' => $this->otherUser->id,
+                'user_email' => $this->otherUser->email,
+                'permissions' => 'read_write',
+            ]);
+    }
+
+    public function testDeletePermission()
+    {
+        $painting = $this->testUser->paintings()->create();
+        $painting->save();
+
+        $this->actingAs($this->testUser)
             ->postJson("/api/p/$painting->id/perms", [
                 'perms' => 'read_write',
                 'email' => $this->otherUser->email,
             ]);
 
-        $add_user_response->assertStatus(200);
-
-        $get_perms_response = $this->actingAs($this->testUser)
-            ->getJson("/api/p/$painting->id/perms");
-        $get_perms_response->assertStatus(200)
-            ->assertJsonFragment(['user_email' => $this->otherUser->email, 'permissions' => 'read_write']);
+        $this->actingAs($this->testUser)
+            ->delete("/api/p/$painting->id/perms/" . $this->otherUser->id)
+             ->dump()
+             ->assertStatus(200);
+        $this->actingAs($this->testUser)
+            ->getJson("/api/p/$painting->id/perms")
+            ->assertStatus(200)
+            ->assertExactJson([]);
     }
 }

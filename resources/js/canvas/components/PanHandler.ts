3d085b56d83935df7b51264bd31aef25dd5cc1c2
Noah Snelson <noah.snelson@protonmail.com>
2020-08-24 22:08:58 -0700

    Fix PanTool to stay within Canvas bounds

--- resources/js/canvas/components/PanHandler.ts
+++ resources/js/canvas/components/PanHandler.ts
@@ -4,7 +4,7 @@ export class PanHandler {
     private shiftedX: number;
     private shiftedY: number;
     private mouseDown: boolean;
-    constructor(){
+    constructor() {
         this.mouseDown = false;
         this.lastX = 0;
         this.lastY = 0;
@@ -14,45 +14,49 @@ export class PanHandler {
     pan(event: any, context: CanvasRenderingContext2D): [number, number] {
         const xCoord = event.clientX / event.scaleFactor;
         const yCoord = event.clientY / event.scaleFactor;
-        if(!this.mouseDown){
+        if (!this.mouseDown) {
             this.mouseDown = true;
             this.lastX = xCoord;
             this.lastY = yCoord;
         }
-        else if(event.type === 'mouseup'){
+        else if (event.type === 'mouseup') {
             this.mouseDown = false;
         }
-        else if(this.mouseDown && event.type === 'mousemove'){
+        else if (this.mouseDown && event.type === 'mousemove') {
             const deltaX = xCoord - this.lastX;
             const deltaY = yCoord - this.lastY;
             this.lastX = xCoord;
             this.lastY = yCoord;
-            if(this.isInsideWidthBounds(this.shiftedX - deltaX, context) &&
-               this.isInsideHeightBounds(this.shiftedY - deltaY, context)){
+            if (this.isInsideWidthBounds(this.shiftedX - deltaX, context, event.scaleFactor) &&
+                this.isInsideHeightBounds(this.shiftedY - deltaY, context, event.scaleFactor)) {
                 this.shiftedX -= deltaX;
                 this.shiftedY -= deltaY;
                 context.translate(deltaX, deltaY);
             }
-            else if(!this.isInsideWidthBounds(this.shiftedX - deltaX, context) &&
-                    this.isInsideHeightBounds(this.shiftedY - deltaY, context)){
+            else if (!this.isInsideWidthBounds(this.shiftedX - deltaX, context, event.scaleFactor) &&
+                this.isInsideHeightBounds(this.shiftedY - deltaY, context, event.scaleFactor)) {
                 this.shiftedY -= deltaY;
                 context.translate(0, deltaY);
             }
-            else if(this.isInsideWidthBounds(this.shiftedX - deltaX, context) &&
-                    !this.isInsideHeightBounds(this.shiftedY - deltaY, context)){
+            else if (this.isInsideWidthBounds(this.shiftedX - deltaX, context, event.scaleFactor) &&
+                !this.isInsideHeightBounds(this.shiftedY - deltaY, context, event.scaleFactor)) {
                 this.shiftedX -= deltaX;
                 context.translate(deltaX, 0);
             }
         }
         return [this.shiftedX, this.shiftedY];
     }
-    isInsideWidthBounds(coord: number, context: CanvasRenderingContext2D): boolean {
-        return (coord > 0 && coord < context.canvas.width);
+    isInsideWidthBounds(coord: number, context: CanvasRenderingContext2D, scale: number): boolean {
+        const viewLeftBound = coord;
+        const viewRightBound = coord + (context.canvas.width / scale);
+        return (viewLeftBound > 0 && viewRightBound < context.canvas.width);
     }
-    isInsideHeightBounds(coord: number, context: CanvasRenderingContext2D): boolean {
-        return (coord > 0 && coord < context.canvas.height);
+    isInsideHeightBounds(coord: number, context: CanvasRenderingContext2D, scale: number): boolean {
+        const viewTopBound = coord;
+        const viewBottomBound = coord + (context.canvas.height / scale);
+        return (viewTopBound > 0 && viewBottomBound < context.canvas.height);
     }
-    resetDistanceShifted(){
+    resetDistanceShifted() {
         this.shiftedY = 0;
         this.shiftedX = 0;
     }

6bed14c256945cbb742375a5981a179b439d94b3
Noah Snelson <noah.snelson@protonmail.com>
2020-09-06 02:14:32 -0700

    Fix asset URL for `logo192.png` on /painting

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1"/>
     <meta name="theme-color" content="#000000"/>
     <meta name="description" content="Web site created using create-react-app"/>
-    <link rel="apple-touch-icon" href="logo192.png"/>
+    <link rel="apple-touch-icon" href={{ asset("logo192.png") }}/>
     <link rel="manifest" href="/manifest.json"/>
     <title>{{ $title }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
